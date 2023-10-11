ad3883925dc06af27220ce11216b0fdbfb8e0f4f
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-11 13:38:42 +0200

    Refactor route handling

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,68 @@
-import { useEffect, useState } from 'react';
-import Signup from './components/Signup/signupPage';
-import Login from './components/Login/loginPage';
 import Navbar from './components/Navbar/navbar';
-import { Outlet } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './components/LandingPage/landingPage';
+import Login from './components/Login/loginPage';
+import Signup from './components/Signup/signupPage';
+import Dashboard from './components/Dashboard/dashboard';
+import ScorecardForm from './components/ScorecardForms/scorecardForm';
 
 export default function App() {
+  const { user, isLoading } = useAuthContext();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <Navbar />
-      <Outlet />
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={user ? <Dashboard /> : <LandingPage />} />
+            <Route
+              path="/login"
+              element={
+                user ? (
+                  <>
+                    <Navigate to="/" />
+                    <Dashboard />
+                  </>
+                ) : (
+                  <Login />
+                )
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                user ? (
+                  <>
+                    <Navigate to="/" />
+                    <Dashboard />
+                  </>
+                ) : (
+                  <Signup />
+                )
+              }
+            />
+            <Route
+              path="/newround"
+              element={
+                user ? (
+                  <ScorecardForm />
+                ) : (
+                  <>
+                    <Navigate to="/login" />
+                    <Login />
+                  </>
+                )
+              }
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,32 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AuthContextProvider } from './context/authContext.jsx';
-import Signup from './components/Signup/signupPage.jsx';
-import Login from './components/Login/loginPage.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/signup',
-        element: <Signup />,
-      },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <App />
     </AuthContextProvider>
   </React.StrictMode>,
 );
