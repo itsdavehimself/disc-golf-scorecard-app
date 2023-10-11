faa56898dcc991e259a8a831ecc8bc9436ec983e
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-11 08:12:20 +0200

    Create authorization middleware

---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const requireAuth = asyncHandler(async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorization token required' });
+  }
+
+  const token = authorization.split(' ')[1];
+  console.log(token);
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+    req.user = await User.findOne({ _id }).select('_id');
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Request is not authorized' });
+  }
+});
+
+module.exports = requireAuth;
