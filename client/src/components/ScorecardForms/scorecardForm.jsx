9f6ea7ea612ab34850990fd71383bcb76237d5db
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-11 20:02:10 +0200

    Create add new scorecard functionality

--- client/src/components/ScorecardForms/scorecardForm.jsx
+++ client/src/components/ScorecardForms/scorecardForm.jsx
@@ -1,42 +1,165 @@
 import { useState, useEffect } from 'react';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function ScorecardForm() {
-  const [course, setCourse] = useState('');
+  const [course, setCourse] = useState(null);
+  const [players, setPlayers] = useState([]);
   const [coursesArr, setCoursesArr] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [friends, setFriends] = useState([]);
+  const [error, setError] = useState(null);
+  const { user } = useAuthContext();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // const currentDateTime = new Date();
+    const currentDateTime = new Date();
+
+    const courseDetailsResponse = await fetch(
+      `http://localhost:8080/api/courses/${course}`,
+    );
+    const courseDetails = await courseDetailsResponse.json();
+
+    if (!courseDetailsResponse.ok) {
+      setError(courseDetails.error);
+      return;
+    }
+
+    const selectedCourse = courseDetails.course;
+
+    const playerObjects = players.map((player) => {
+      const playerScores = selectedCourse.holes.map((hole) => ({
+        holeNumber: hole.holeNumber,
+        holePar: hole.par,
+        score: null,
+      }));
+
+      return {
+        type: player.type,
+        reference: player.reference,
+        scores: playerScores,
+      };
+    });
+
+    const scorecard = {
+      course,
+      date: currentDateTime,
+      startTime: currentDateTime,
+      players: playerObjects,
+    };
+
+    const response = await fetch('http://localhost:8080/api/scorecards', {
+      method: 'POST',
+      body: JSON.stringify(scorecard),
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      setError(null);
+      console.log('new scorecard added');
+      setCourse(null);
+      setPlayers([]);
+    }
+  };
+
+  const handleCheckboxChange = (e) => {
+    const value = e.target.value;
+    const playerId = value;
+    const playerType = e.target.name;
+
+    if (e.target.checked) {
+      const playerObject = {
+        type: playerType,
+        reference: playerId,
+        scores: [],
+      };
+      setPlayers((prevPlayers) => [...prevPlayers, playerObject]);
+    } else {
+      setPlayers((prevPlayers) =>
+        prevPlayers.filter((player) => player.reference !== playerId),
+      );
+    }
+  };
 
   useEffect(() => {
-    async function fetchCourses() {
+    async function fetchData() {
       try {
-        const response = await fetch('http://localhost:8080/api/courses');
-        if (response.ok) {
-          const courses = await response.json();
-          setCoursesArr(courses);
-          setIsLoading(false);
-        }
+        const coursesResponse = fetch('http://localhost:8080/api/courses');
+        const friendsResponse = fetch('http://localhost:8080/api/friends', {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+
+        const [courses, friends] = await Promise.all([
+          coursesResponse,
+          friendsResponse,
+        ]).then((responses) => Promise.all(responses.map((res) => res.json())));
+
+        setCoursesArr(courses);
+        setCourse(courses[0]._id);
+        setFriends(friends);
+        setIsLoading(false);
       } catch (error) {
-        throw Error(error);
+        console.error(error);
       }
     }
-    fetchCourses();
-  }, []);
+
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <form className="create">
+    <form className="create" onSubmit={handleSubmit}>
       <h3>Select course</h3>
       <label>Course: </label>
-      <select>
+      <select onChange={(e) => setCourse(e.target.value)}>
         {coursesArr.map((courseItem) => (
-          <option key={courseItem._id} value={courseItem.name}>
+          <option key={courseItem._id} value={courseItem._id}>
             {courseItem.name}
           </option>
         ))}
       </select>
       <h3>Who&apos;s playing?</h3>
+      <div className="users-playing">
+        <label>
+          <input
+            type="checkbox"
+            name="User"
+            id="player1"
+            value={user.user._id}
+            onChange={handleCheckboxChange}
+          ></input>
+          {user.user.username}
+        </label>
+        {friends.map((friend, index) => (
+          <label key={friend._id}>
+            <input
+              type="checkbox"
+              name="Friend"
+              id={`player${index + 2}`}
+              value={friend._id}
+              onChange={handleCheckboxChange}
+            />
+            {friend.name}
+          </label>
+        ))}
+      </div>
+      <button>Start round</button>
     </form>
   );
 }
