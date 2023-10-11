2702bbf241f2db6a14a03de04c81a67857b1b761
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-11 08:18:11 +0200

    Remove console log

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -10,7 +10,6 @@ const requireAuth = asyncHandler(async (req, res, next) => {
   }
 
   const token = authorization.split(' ')[1];
-  console.log(token);
 
   try {
     const { _id } = jwt.verify(token, process.env.SECRET);
