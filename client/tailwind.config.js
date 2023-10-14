d7f475f06e812916102826ac9ac36994492eb30c
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-14 08:09:49 +0200

    Add tailwind dependencies

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      emerald: '#27CE8E',
+      honeydew: '#EBFFF7',
+      'black-olive': '#29332F',
+      jade: '#22A875',
+      'off-white': '#FCFFFE',
+      'white-smoke': '#F5F5F5',
+      transparent: 'rgba(0,0,0,0)',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern':
+          "linear-gradient(to right bottom, rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/public/images/home-bg-med.jpg')",
+      },
+      backgroundSize: {
+        90: '90%',
+        50: '50%',
+        75: '75%',
+        200: '200%',
+        cover: 'cover',
+        contain: 'contain',
+      },
+    },
   },
   plugins: [],
 };
