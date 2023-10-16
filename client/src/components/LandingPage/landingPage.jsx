74d4a8a681e25bbbba3e8703de30fe8b65ce6dbc
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-16 12:01:24 +0200

    Style landing page

--- client/src/components/LandingPage/landingPage.jsx
+++ client/src/components/LandingPage/landingPage.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
 export default function LandingPage() {
   return (
-    <div>
-      <h1>ChainSeeker</h1>
-      <h3>Your disc golf scorecard companion</h3>
+    <div className="flex flex-col justify-center items-center bg-hero-pattern h-[80vh] w-screen bg-cover bg-bottom md:bg-cover md:bg-center ">
+      <h2 className=" text-off-white text-4xl font-semibold text-center px-1 pb-5">
+        Your disc golf scorecard companion
+      </h2>
+      <p className="text-off-white text-lg px-5 text-center pb-5">
+        Elevate your disc golf game and keep track of your best rounds. Tee off
+        to a new level of fun!
+      </p>
+      <Link to={'/signup'}>
+        <button className="bg-jade px-4 py-3 text-xl rounded-md text-off-white font-semibold cursor-pointer hover:bg-emerald transition-colors ">
+          Get Started
+        </button>
+      </Link>
     </div>
   );
 }
