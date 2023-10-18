ffb63c52248b06d31cdb1ae598a0bc972b679445
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-18 11:31:27 +0200

    Add friend list context provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import './index.css';
 import { AuthContextProvider } from './context/authContext.jsx';
+import { FriendListContextProvider } from './context/friendContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <FriendListContextProvider>
+        <App />
+      </FriendListContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 );
