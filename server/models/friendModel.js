a4ab02e206d96d36855b9104322620711c07da81
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-18 07:09:57 +0200

    Update name minimum length requirement

--- server/models/friendModel.js
+++ server/models/friendModel.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const FriendSchema = new Schema({
-  name: { type: String, required: true, maxLength: 15 },
+  name: {
+    type: String, required: true, minLength: 1, maxLength: 15,
+  },
   scorecards: [{ type: Schema.Types.ObjectId, ref: 'Scorecard', required: false }],
   createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
