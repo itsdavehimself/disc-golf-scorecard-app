74a7f76aeec9a8151189e5c7205e995143dc8dd1
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-19 15:27:50 +0200

    Create add friend functionality

--- client/src/components/ScorecardForms/scorecardForm.jsx
+++ client/src/components/ScorecardForms/scorecardForm.jsx
@@ -117,7 +117,7 @@ export default function ScorecardForm() {
       const playerScores = selectedCourse.holes.map((hole) => ({
         holeNumber: hole.holeNumber,
         holePar: hole.par,
-        score: null,
+        score: 0,
       }));
 
       return {
@@ -272,6 +272,13 @@ export default function ScorecardForm() {
     setNewFriendNameError(false);
   });
 
+  const handleCancelAddFriend = (e) => {
+    e.preventDefault();
+    setAddFriendOpen(false);
+    setNewFriendName(null);
+    setNewFriendNameError(false);
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -282,30 +289,42 @@ export default function ScorecardForm() {
         <div className="flex items-center justify-center absolute z-50 w-screen h-screen bg-modal">
           <div
             ref={outsideAddFriend}
-            className="relative w-full h-2/3 mx-4 bg-off-white rounded-md"
+            className="relative flex flex-col justify-center gap-6 py-5 px-5 w-full h-max mx-4 bg-off-white rounded-md"
           >
-            <div>Add friend</div>
-            <p>
-              Create a profile to track scores and statistics for your friend.
-            </p>
-            <form onSubmit={handleNewFriendSubmit}>
-              <label className="text-black-olive">Friend&apos;s name</label>
-              <input
-                onChange={handleFriendNameChange}
-                type="text"
-                className={`${
-                  newFriendNameError
-                    ? 'ring-red ring-2'
-                    : 'border-white-smoke border focus:ring-2 focus:ring-jade'
-                } text-black-olive rounded-md shadow-md p-2 focus:outline-none`}
-              ></input>
-              {newFriendNameError && (
-                <div className="text-sm pt-1 text-vermillion">
-                  Please enter a name
+            <div className="flex flex-col">
+              <div className="text-xl font-semibold">Add friend</div>
+              <p className="text-sm pt-1">
+                Create a profile to track scores and statistics for your friend.
+              </p>
+            </div>
+            <form
+              className="flex flex-col gap-2 justify-center"
+              onSubmit={handleNewFriendSubmit}
+            >
+              <div className="flex flex-col">
+                <label className="text-black-olive text-sm">Player name</label>
+                <input
+                  onChange={handleFriendNameChange}
+                  type="text"
+                  className={`${
+                    newFriendNameError
+                      ? 'ring-red ring-2'
+                      : 'border-white-smoke border focus:ring-2 focus:ring-jade'
+                  } text-black-olive rounded-md shadow-md p-2 focus:outline-none`}
+                ></input>
+                <div className="h-5">
+                  {newFriendNameError && (
+                    <div className="h-max text-sm pt-1 text-vermillion">
+                      Please enter a name
+                    </div>
+                  )}
                 </div>
-              )}
+              </div>
               <button className="bg-jade py-3 rounded-md text-off-white font-semibold cursor-pointer hover:bg-emerald transition-colors">
                 Add friend
+              </button>
+              <button onClick={handleCancelAddFriend} className="py-3">
+                Cancel
               </button>
             </form>
           </div>
