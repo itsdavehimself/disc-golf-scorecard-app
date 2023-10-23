812d5dcdafe9f63743a03d12a4ec2d3b540cfac8
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-23 08:55:17 +0200

    Extract player performance data

--- client/src/components/FriendProfile/FriendProfile.jsx
+++ client/src/components/FriendProfile/FriendProfile.jsx
@@ -12,6 +12,13 @@ export default function FriendProfile() {
   const [userWins, setUserWins] = useState(0);
   const [friendWins, setFriendWins] = useState(0);
   const [ties, setTies] = useState(0);
+  const [aces, setAces] = useState(0);
+  const [eagles, setEagles] = useState(0);
+  const [birdies, setBirdies] = useState(0);
+  const [pars, setPars] = useState(0);
+  const [bogey, setBogey] = useState(0);
+  const [doubleBogeys, setDoubleBogeys] = useState(0);
+  const [tripleBogeys, setTripleBogeys] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,10 +46,12 @@ export default function FriendProfile() {
         ]).then((responses) => Promise.all(responses.map((res) => res.json())));
 
         setFriend(friend.friend);
+        const totalScorecards = scorecards.scorecards;
         setTotalScorecards(scorecards.scorecards);
         const scorecardsWithUser = scorecards.scorecards.filter((scorecard) => {
           return scorecard.players.some((player) => player.type === 'User');
         });
+
         setScorecardsWithUser(scorecardsWithUser);
         const userScoresArr = [];
         const friendScoresArr = [];
@@ -84,9 +93,65 @@ export default function FriendProfile() {
             ties++;
           }
         }
+
+        const scoresArray = [];
+        const parArray = [];
+
+        totalScorecards.forEach((scorecard) => {
+          scorecard.players.forEach((player) => {
+            if (player.reference === id) {
+              player.scores.forEach((score) => {
+                scoresArray.push(score.score);
+              });
+              player.scores.forEach((score) => {
+                parArray.push(score.holePar);
+              });
+            }
+          });
+        });
+
+        let acesCount = 0;
+        let eaglesCount = 0;
+        let birdiesCount = 0;
+        let parsCount = 0;
+        let bogeyCount = 0;
+        let dblBogeyCount = 0;
+        let trpBogeyCount = 0;
+
+        for (
+          let i = 0;
+          i < Math.min(scoresArray.length, parArray.length);
+          i++
+        ) {
+          const score = scoresArray[i];
+          const par = parArray[i];
+
+          if (score === 1) {
+            acesCount++;
+          } else if (score === par - 2) {
+            eaglesCount++;
+          } else if (score === par - 1) {
+            birdiesCount++;
+          } else if (score === par) {
+            parsCount++;
+          } else if (score === par + 1) {
+            bogeyCount++;
+          } else if (score === par + 2) {
+            dblBogeyCount++;
+          } else if (score === par + 3) {
+            trpBogeyCount++;
+          }
+        }
         setUserWins(userPoints);
         setFriendWins(friendPoints);
         setTies(ties);
+        setAces(acesCount);
+        setBirdies(birdiesCount);
+        setEagles(eaglesCount);
+        setPars(parsCount);
+        setBogey(bogeyCount);
+        setDoubleBogeys(dblBogeyCount);
+        setTripleBogeys(trpBogeyCount);
         setIsLoading(false);
       } catch (error) {
         console.log(error);
@@ -94,7 +159,7 @@ export default function FriendProfile() {
     };
 
     fetchData();
-  }, [id, user]);
+  }, [id, user, totalScorecards]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -105,8 +170,14 @@ export default function FriendProfile() {
       <div className="pt-16">
         <div className="text-2xl font-semibold">{friend.name}</div>
         <div className="text-sm">
-          <div>Has played in {totalScorecards.length} rounds total</div>
-          <div>Has played in {scorecardsWithUser.length} rounds with you</div>
+          <div>
+            Has played in {totalScorecards.length}
+            {totalScorecards.length === 1 ? ' round' : ' rounds'} total
+          </div>
+          <div>
+            Has played in {scorecardsWithUser.length}{' '}
+            {scorecardsWithUser.length === 1 ? ' round' : ' rounds'} with you
+          </div>
         </div>
         <div className="pt-6">
           <div className="text-center text-sm font-semibold">
