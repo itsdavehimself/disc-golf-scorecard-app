e1f8cb26ab0e40b54b8a60fef7a930dae3b4f0dc
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-25 13:01:57 +0200

    Add styling

--- client/src/components/CourseCard/courseCard.jsx
+++ client/src/components/CourseCard/courseCard.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faThumbTack } from '@fortawesome/free-solid-svg-icons';
 
 export default function CourseCard({ course, searchValueInput }) {
   return (
@@ -11,14 +13,31 @@ export default function CourseCard({ course, searchValueInput }) {
           : 'hidden'
       }`}
     >
-      <div className={`w-full bg-white`}>
+      <div className="w-full bg-white">
         <div>
-          <span className="font-semibold">{course.name}</span> -{' '}
-          {course.holes.length} Holes
+          <img
+            className="rounded-md h-40 w-full object-cover"
+            src={course.image}
+          ></img>
         </div>
-        <div className="text-xs">
-          {course.city}, {course.state}
+        <div className="flex justify-between items-center pt-1">
+          <div className="text-xl font-semibold">{course.name}</div>
+          <div className="bg-jade font-semibold text-off-white rounded-2xl px-2 py-0.5">
+            {course.rating}
+          </div>
         </div>
+        <div className="flex text-sm py-1 gap-2">
+          <div>
+            <FontAwesomeIcon icon={faLocationDot} className="text-jade pr-1" />
+            {course.city}, {course.state}
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faThumbTack} className="text-jade pr-1" />
+            {course.holes.length} holes
+          </div>
+        </div>
+        <div className="hidden md:block">{course.description}</div>
+        <div className="block md:hidden text-xs">{course.blurb}</div>
       </div>
     </div>
   );
