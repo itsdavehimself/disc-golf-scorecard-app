7a04fa3e4d4a31ce9effba9fa85842efb95bdf13
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-25 19:35:34 +0200

    Add prop for confirm message

--- client/src/components/ConfirmDeleteModal/confirmDeleteModal.jsx
+++ client/src/components/ConfirmDeleteModal/confirmDeleteModal.jsx
@@ -4,6 +4,7 @@ export default function ConfirmDeleteModal({
   outsideConfirmDelete,
   setIsConfirmOpen,
   handleConfirmDelete,
+  nameForModal,
 }) {
   return (
     <div className="flex items-center justify-center absolute z-50 w-screen h-screen bg-modal text-black-olive">
@@ -12,7 +13,7 @@ export default function ConfirmDeleteModal({
         className="relative flex flex-col justify-center gap-6 py-5 px-5 w-2/3 h-max mx-4 bg-off-white rounded-md"
       >
         <p className="text-center">
-          Are you sure you want to delete this scorecard?
+          Are you sure you want to delete this {nameForModal}?
         </p>
         <div className="grid grid-rows-auto gap-2 justify-center items-center w-full">
           <button
@@ -36,6 +37,7 @@ export default function ConfirmDeleteModal({
 ConfirmDeleteModal.propTypes = {
   setIsConfirmOpen: PropTypes.func,
   handleConfirmDelete: PropTypes.func,
+  nameForModal: PropTypes.string,
   outsideConfirmDelete: PropTypes.shape({
     current: PropTypes.instanceOf(Element),
   }),
