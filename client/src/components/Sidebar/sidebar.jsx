ac23afbd0a49dfee0c7e02cc1dd2f78a48a31862
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-30 12:29:59 +0100

    Update links

--- client/src/components/Sidebar/sidebar.jsx
+++ client/src/components/Sidebar/sidebar.jsx
@@ -1,10 +1,34 @@
+import { Link } from 'react-router-dom';
+
 export default function Sidebar() {
   return (
     <div className="bg-white md:w-80">
       <div className="hidden md:flex flex-col pt-16 items-start pl-5">
-        <button>Scorecards</button>
-        <button>Friends</button>
-        <button>Courses</button>
+        <Link to={'/'}>
+          <button className=" text-black-olive  hover:text-jade px-3 py-2 cursor-pointer rounded-md">
+            Dashboard
+          </button>
+        </Link>
+        <Link to={'/scorecards'}>
+          <button className=" text-black-olive  hover:text-jade px-3 py-2 cursor-pointer rounded-md">
+            Scorecards
+          </button>
+        </Link>
+        <Link to="/mystats">
+          <button className="text-black-olive hover-text-jade px-3 py-2 cursor-pointer rounded-md">
+            My Stats
+          </button>
+        </Link>
+        <Link to={'/friends'}>
+          <button className="text-black-olive hover-text-jade px-3 py-2 cursor-pointer rounded-md">
+            Friends
+          </button>
+        </Link>
+        <Link to={'/courses'}>
+          <button className="text-black-olive hover-text-jade px-3 py-2 cursor-pointer rounded-md">
+            Courses
+          </button>
+        </Link>
       </div>
     </div>
   );
