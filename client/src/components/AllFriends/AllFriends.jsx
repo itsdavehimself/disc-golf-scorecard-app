2494c74f77aae87d15d56279be7201e883d388ff
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-30 16:28:17 +0100

    Add styles

--- client/src/components/AllFriends/AllFriends.jsx
+++ client/src/components/AllFriends/AllFriends.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUser,
+  faMagnifyingGlass,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
 export default function AllFriends() {
@@ -47,56 +51,73 @@ export default function AllFriends() {
   }
 
   return (
-    <div className="flex flex-col bg-off-white w-full px-4 pt-16">
-      <div className="flex flex-row items-center justify-between pt-4 pb-2">
-        <div className="flex text-sm text-black items-center font-semibold">
-          Your friends
+    <div className="flex flex-col bg-off-white w-full px-3 pt-16">
+      <div className="bg-white rounded-lg shadow-lg mt-3">
+        <div className="flex flex-row items-end justify-between pb-2">
+          <div className="text-lg pt-3 px-3 text-black font-semibold">
+            Your friends
+          </div>
+          <div className="flex pr-2 text-sm">{friends.length} players</div>
         </div>
-        <div className="text-sm">{friends.length} players</div>
-      </div>
-      <div className="flex items-center justify-center bg-honeydew pl-2 sticky top-0">
-        <FontAwesomeIcon
-          icon={faMagnifyingGlass}
-          className="text-sm text-black"
-        />
-        <input
-          type="text"
-          onChange={(e) => {
-            setSearchValueInput(e.target.value);
-          }}
-          value={searchValueInput}
-          placeholder="Search friend"
-          className="bg-honeydew w-full p-1 outline-none pl-2"
-        ></input>
-      </div>
-      <div>
-        {friends &&
-          friends.map((friend) => (
-            <div
-              onClick={() => openFriend(friend._id)}
-              className={`p-2 my-2 bg-white rounded-md shadow-sm text-black text-sm hover:cursor-pointer active:bg-honeydew ${
-                friend.name
-                  .toLowerCase()
-                  .startsWith(searchValueInput.toLowerCase())
-                  ? 'block'
-                  : 'hidden'
-              }`}
-              key={friend._id}
-            >
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    className="text-xs bg-jade text-off-white px-2 py-2 rounded-full"
-                  />
-                  <div className="text-black font-semibold">{friend.name}</div>
-                </div>
-                <div className="text-black pr-2">
-                  {friend.scorecards.length} rounds
+        <div className="flex items-center justify-center bg-off-white pl-2 mx-3 rounded-lg">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="text-sm text-black"
+          />
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearchValueInput(e.target.value);
+            }}
+            value={searchValueInput}
+            placeholder="Search friend"
+            className="bg-off-white w-full p-1 outline-none pl-2 rounded-lg"
+          ></input>
+        </div>
+        <div>
+          {friends &&
+            friends.map((friend) => (
+              <div
+                onClick={() => openFriend(friend._id)}
+                className={`my-2 mx-2 px-3 bg-white rounded-lg text-black text-sm hover:cursor-pointer hover:bg-white-smoke transition-colors group ${
+                  friend.name
+                    .toLowerCase()
+                    .startsWith(searchValueInput.toLowerCase())
+                    ? 'block'
+                    : 'hidden'
+                }`}
+                key={friend._id}
+              >
+                <div className="flex items-center justify-between border-b border-white-smoke pb-3 pt-2">
+                  <div className="flex flex-col">
+                    <div className="flex items-center gap-2">
+                      <FontAwesomeIcon
+                        icon={faUser}
+                        className=" bg-off-white text-gray px-2 py-2 rounded-full"
+                      />
+                      <div>
+                        <div className="text-black font-semibold">
+                          {friend.name}
+                        </div>
+                        <div className="text-black pr-2 text-xs">
+                          {friend.scorecards.length} rounds
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-end pr-2">
+                    <FontAwesomeIcon
+                      icon={faAngleRight}
+                      className="text-md text-gray group-hover:text-jade transition"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
+        <div className="flex items-center justify-center pt-2 pb-4">
+          Chainseeker
+        </div>
       </div>
     </div>
   );
