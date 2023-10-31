2f92395e0e0e84e3887bcc0eadfe41af06ee8faf
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-10-31 15:12:51 +0100

    Add vertical ellipsis button

--- client/src/components/FriendProfile/FriendProfile.jsx
+++ client/src/components/FriendProfile/FriendProfile.jsx
@@ -14,7 +14,11 @@ import {
 } from '../../utilities/friendProfileDataUtilities';
 import YearFilterModal from '../YearFilterModal/yearFilterModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUser,
+  faAngleRight,
+  faEllipsisVertical,
+} from '@fortawesome/free-solid-svg-icons';
 
 let useClickOutside = (handler) => {
   const domNode = useRef();
@@ -502,24 +506,33 @@ export default function FriendProfile() {
       )}
       <div className="flex flex-col bg-off-white w-full px-3 text-black">
         <div className="pt-16">
-          <div className="bg-white rounded-lg shadow-lg px-3 my-3 py-2">
-            <div className="text-2xl font-semibold">{friend.name}</div>
-            <div className="text-sm">
-              <div className="pt-1.5">
-                Has played in{' '}
-                <span className="font-semibold">
-                  {totalScorecards.length}
-                  {totalScorecards.length === 1 ? ' round' : ' rounds'} total
-                </span>
+          <div className="grid grid-cols-10 bg-white rounded-lg shadow-lg px-3 my-3 py-2">
+            <div className="col-start-1 col-end-10">
+              <div className="text-2xl font-semibold">{friend.name}</div>
+              <div className="text-sm">
+                <div className="pt-1.5">
+                  Has played in{' '}
+                  <span className="font-semibold">
+                    {totalScorecards.length}
+                    {totalScorecards.length === 1 ? ' round' : ' rounds'} total
+                  </span>
+                </div>
+                <div>
+                  Has played in{' '}
+                  <span className="font-semibold">
+                    {scorecardsWithUser.length}
+                    {scorecardsWithUser.length === 1
+                      ? ' round'
+                      : ' rounds'}{' '}
+                    with you
+                  </span>
+                </div>
               </div>
-              <div>
-                Has played in{' '}
-                <span className="font-semibold">
-                  {scorecardsWithUser.length}
-                  {scorecardsWithUser.length === 1 ? ' round' : ' rounds'} with
-                  you
-                </span>
-              </div>
+            </div>
+            <div className="flex justify-end text-lg">
+              <button className="h-6 w-6">
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
             </div>
           </div>
 
