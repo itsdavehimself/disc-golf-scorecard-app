3bfa8b0361d8dcac05efa13052ceee829fd0467c
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-11-02 16:06:45 +0100

    Change logo size

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -4,8 +4,8 @@ function Logo(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="28.000000pt"
-      height="28.000000pt"
+      width="24.000000pt"
+      height="24.000000pt"
       viewBox="0 0 1500.000000 1500.000000"
       {...props}
     >
