291fb0eb70b72df698086d2105f03c70db673ecd
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-11-03 10:48:05 +0100

    Make sidebar sticky

--- client/src/components/Sidebar/sidebar.jsx
+++ client/src/components/Sidebar/sidebar.jsx
@@ -3,32 +3,34 @@ import { Link } from 'react-router-dom';
 export default function Sidebar() {
   return (
     <div className="bg-white md:w-80">
-      <div className="hidden md:flex flex-col pt-16 items-start pl-5">
-        <Link to={'/'}>
-          <button className=" text-black  hover:text-jade px-3 py-2 cursor-pointer rounded-md">
-            Dashboard
-          </button>
-        </Link>
-        <Link to={'/scorecards'}>
-          <button className=" text-black  hover:text-jade px-3 py-2 cursor-pointer rounded-md">
-            Scorecards
-          </button>
-        </Link>
-        <Link to="/mystats">
-          <button className="text-black hover-text-jade px-3 py-2 cursor-pointer rounded-md">
-            My Stats
-          </button>
-        </Link>
-        <Link to={'/friends'}>
-          <button className="text-black hover-text-jade px-3 py-2 cursor-pointer rounded-md">
-            Friends
-          </button>
-        </Link>
-        <Link to={'/courses'}>
-          <button className="text-black hover-text-jade px-3 py-2 cursor-pointer rounded-md">
-            Courses
-          </button>
-        </Link>
+      <div className="hidden md:flex flex-col pt-16 items-start pl-5 h-full">
+        <div className="md:flex flex-col md:sticky top-16">
+          <Link to={'/'}>
+            <button className=" text-black  hover:text-jade px-3 py-2 cursor-pointer rounded-md">
+              Dashboard
+            </button>
+          </Link>
+          <Link to={'/scorecards'}>
+            <button className=" text-black  hover:text-jade px-3 py-2 cursor-pointer rounded-md">
+              Scorecards
+            </button>
+          </Link>
+          <Link to="/mystats">
+            <button className="text-black hover-text-jade px-3 py-2 cursor-pointer rounded-md">
+              My Stats
+            </button>
+          </Link>
+          <Link to={'/friends'}>
+            <button className="text-black hover-text-jade px-3 py-2 cursor-pointer rounded-md">
+              Friends
+            </button>
+          </Link>
+          <Link to={'/courses'}>
+            <button className="text-black hover-text-jade px-3 py-2 cursor-pointer rounded-md">
+              Courses
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
