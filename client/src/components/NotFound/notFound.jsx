46739b00391fae7ca64537ca34ee6e0f1f36ea0c
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-18 14:46:21 -0600

    Add responsive styles

--- client/src/components/NotFound/notFound.jsx
+++ client/src/components/NotFound/notFound.jsx
@@ -2,13 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-10 w-full justify-center items-center text-black">
-      <h1 className="text-4xl font-semibold">Uh oh! Looks like you wandered off the fairway.</h1>
-      <h2 className="text-2xl">This page doesn't exist.</h2>
-      <Link
-            to="/"
-          >
-      <button className="bg-jade py-2 px-3 rounded-md text-white font-semibold cursor-pointer hover:bg-emerald transition-colors">Take me home</button>
+    <div className="flex flex-col gap-10 w-full justify-center text-center text-black">
+      <h1 className="text-2xl md:text-4xl font-semibold">Uh oh! Looks like you wandered off the fairway.</h1>
+      <h2 className="text-xl md:text-2xl">This page doesn't exist.</h2>
+      <Link to="/">
+        <button className="bg-jade py-2 px-3 rounded-md text-white font-semibold cursor-pointer hover:bg-emerald transition-colors">Take me home</button>
       </Link>
     </div>
   );
