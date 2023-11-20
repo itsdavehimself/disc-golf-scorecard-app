abc173359a88c3e26ac0e6cdd0ee0d0354c6ed06
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-20 12:21:49 -0600

    Create review card for landing page

---
+++ client/src/components/ReviewCard.jsx
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+
+export default function ReviewCard({ review, username }) {
+  return (
+    <div className="flex flex-col justify-between h-48 w-4/5 md:h-64 md:w-80 py-4 px-4 bg-white rounded-xl border-2 border-white-smoke shadow-lg">
+      <div className="flex flex-col gap-2">
+        <div className="text-jade text-lg">
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+        </div>
+        <div className="text-black italic">&quot;{review}&quot;</div>
+      </div>
+      <div className="text-gray">{username}</div>
+    </div>
+  );
+}
+
+ReviewCard.propTypes = {
+  review: PropTypes.string,
+  username: PropTypes.username,
+};
