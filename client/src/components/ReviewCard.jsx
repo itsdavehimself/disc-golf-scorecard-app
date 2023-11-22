9b92990c0981b31d86ad7d86e6ac86b0537d2a5c
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-22 11:57:31 -0600

    Change card height

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 export default function ReviewCard({ review, username }) {
   return (
-    <section className="flex flex-col justify-between h-48 w-4/5 md:h-64 md:w-80 py-4 px-4 bg-white rounded-xl border-2 border-white-smoke shadow-lg">
+    <section className="flex flex-col justify-between h-auto w-4/5 md:h-64 md:w-80 py-4 px-4 bg-white rounded-xl border-2 border-white-smoke shadow-lg">
       <main className="flex flex-col gap-2">
         <div className="text-jade text-lg">
           <FontAwesomeIcon icon={faStar} />
@@ -15,7 +15,7 @@ export default function ReviewCard({ review, username }) {
         </div>
         <div className="text-black italic">&quot;{review}&quot;</div>
       </main>
-      <div className="text-gray">{username}</div>
+      <div className="text-gray pt-4 md:pt-0">{username}</div>
     </section>
   );
 }
