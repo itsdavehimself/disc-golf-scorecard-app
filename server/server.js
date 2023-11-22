1c204d8444e61326c0284c8fb4ae78ab0bc89b9f
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-22 09:58:25 -0600

    Update listening port

--- server/server.js
+++ server/server.js
@@ -14,15 +14,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: true }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(process.env.URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('DB CONNECTED')).catch((err) => console.log('DB Connection Error', err));
+mongoose
+  .connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB CONNECTED'))
+  .catch((err) => console.log('DB Connection Error', err));
 
 app.use(cors({ origin: true, credentials: true }));
 
@@ -36,4 +45,6 @@ app.use('/api/friends', friendRoutes);
 
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Server is running on ${port}`));
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening at http://0.0.0.0:${port}`);
+});
