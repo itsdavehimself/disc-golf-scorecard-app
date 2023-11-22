479f165bce797dd6dc7d1b1a4a6cb935ebb604f7
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-22 10:49:04 -0600

    Update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,10 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
 });
